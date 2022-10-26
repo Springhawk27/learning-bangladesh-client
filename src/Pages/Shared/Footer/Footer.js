c0ee14569c309302bf6a9349796105e85326c9e0
Springhawk27 <sajjad0mahmud@gmail.com>
2022-10-26 22:38:05 +0600

    footer implemented

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from "./../../../assets/logo.png"
 
 const Footer = () => {
     return (
         <div>
-            <h2>This is footer</h2>
-        </div>
+            <footer className="text-gray-600 body-font">
+                <div className="container px-12 py-12 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col border-t-2">
+                    <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+                        <Link to="/home" className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                            <img src={logo} alt="" style={{ width: '100px' }} />
+
+                            <span className="ml-3 text-lg text-blue-800">Learning Bangladesh</span>
+                        </Link>
+                        <p className="mt-2 text-sm text-gray-700">We are here to give you the best specialization courses with the lowest available price.</p>
+                    </div>
+                    <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+                        <div className="lg:w-2/3 md:w-1/3 w-full px-4">
+                            <h2 className="title-font font-medium text-blue-600 tracking-widest text-sm mb-3">Contact Us</h2>
+                            <nav className="list-none mb-4">
+                                <p className="mt-2 text-sm text-gray-700">Road #02, Sector #04, Uttara, Dhaka-1230, Bangladesh
+                                </p>
+                                <p className="mt-2 text-sm text-gray-700">Phone : 09621 123456, 09621 123457
+                                </p>
+                                <p className="mt-2 text-sm text-gray-700">E-mail : info@learningbangladesh.com
+                                </p>
+
+                            </nav>
+                        </div>
+                        <div className="lg:w-1/3 md:w-1/3 w-full px-4">
+                            <h2 className="title-font font-medium text-blue-600 tracking-widest text-sm mb-3">Quick Links</h2>
+                            <nav className="list-none mb-2">
+                                <li>
+                                    <Link to="/courses" className="text-gray-600 hover:text-green-500">Courses</Link>
+                                </li>
+
+                            </nav>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="bg-blue-900">
+                    <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+                        <p className="text-white text-sm text-center sm:text-left">© 2022 Learning Bangladesh
+                        </p>
+
+                    </div>
+                </div>
+            </footer >
+        </div >
     );
 };
 
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -7,84 +7,8 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
 
-        // <Navbar
-        //     fluid={true}
-        //     rounded={true}
-        //     className="dark:text-white"
-        // >
-        //     <Link to="/" className='flex'>
-        //         <img className="mr-3 h-6 sm:h-9"
-        //             src={logo} alt="" style={{ width: '100px' }} />
 
-        //         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white text-blue-800">
-        //             Learning Bangladesh
-        //         </span>
-        //     </Link>
-        //     <div className="flex md:order-2">
-        //         <Dropdown
-        //             arrowIcon={false}
-        //             inline={true}
-        //             label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />}
-        //         >
-        //             <Dropdown.Header>
-        //                 <span className="block text-sm">
-        //                     Bonnie Green
-        //                 </span>
-        //                 <span className="block truncate text-sm font-medium">
-        //                     name@flowbite.com
-        //                 </span>
-        //             </Dropdown.Header>
-        //             <Dropdown.Item>
-        //                 Dashboard
-        //             </Dropdown.Item>
-        //             <Dropdown.Item>
-        //                 Settings
-        //             </Dropdown.Item>
-        //             <Dropdown.Item>
-        //                 Earnings
-        //             </Dropdown.Item>
-        //             <Dropdown.Divider />
-        //             <Dropdown.Item>
-        //                 Sign out
-        //             </Dropdown.Item>
-        //         </Dropdown>
-        //         <Navbar.Toggle />
-        //     </div>
-        //     <Navbar.Collapse>
-        //         <Link
-        //             to="/"
-        //             active={true}
-        //         >
-        //             Home
-        //         </Link>
-        //         <Link
-        //             to="/courses"
-        //         >
-        //             Courses
-        //         </Link>
-        //         <Link
-        //             to="/faq"
-        //         >
-        //             FAQ
-        //         </Link>
-        //         <Link
-        //             to="/blog"
-        //         >
-        //             Blog
-        //         </Link>
-
-
-
-
-        //     </Navbar.Collapse>
-        //     <Flowbite>
-        //         <DarkThemeToggle />
-        //     </Flowbite>
-
-
-        // </Navbar>
-
-        <header className=" body-font ">
+        <header className=" body-font bg-gray-100 sticky top-0 z-50">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <Link to="/home" className="flex title-font font-medium items-center  mb-4 md:mb-0">
 
