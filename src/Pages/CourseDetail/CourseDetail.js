ce87f35ed112625e31b905512878e2ae3b0ee17b
Springhawk27 <sajjad0mahmud@gmail.com>
2022-10-27 21:45:46 +0600

    react to print functionality added

--- src/Pages/CourseDetail/CourseDetail.js
+++ src/Pages/CourseDetail/CourseDetail.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
-import { AiFillStar } from 'react-icons/ai';
+import { AiFillStar, AiOutlineDownload } from 'react-icons/ai';
+import { Button } from 'flowbite-react';
+import ReactToPrint from 'react-to-print';
 
 
 
 const CourseDetail = () => {
+
+    const ref = useRef();
 
     const course = useLoaderData();
     console.log(course)
@@ -12,12 +16,38 @@ const CourseDetail = () => {
 
     return (
         <section className="text-gray-600 body-font overflow-hidden">
-            <div className="container px-5 py-24 mx-auto">
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 pt-8 mx-auto">
+                    <div className="flex flex-col text-center w-full mb-4 ">
+
+                        <div className='flex justify-between items-center w-2/4 mx-auto'>
+                            <h1 className="sm:text-2xl text-xl font-medium title-font mb-4 text-blue-900">Welcome to {name} course</h1>
+                            <ReactToPrint
+                                trigger={() => <Button
+                                    outline={true}
+                                    gradientDuoTone="purpleToBlue"
+                                >
+                                    <AiOutlineDownload className='text-xl'></AiOutlineDownload>
+                                </Button>}
+                                content={() => ref.current}
+                            ></ReactToPrint>
+                        </div>
+                        <hr className='border-b-2 border-blue-900 w-1/5 mx-auto mt-2' />
+
+                    </div>
+
+                </div>
+            </section>
+
+            <div ref={ref} className="container px-5 pt-12  mx-auto">
                 <div className="lg:w-4/5 mx-auto flex flex-wrap">
                     <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-contain object-center rounded" src={img} />
                     <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
 
-                        <h1 className="text-blue-900 text-2xl title-font font-medium mb-1 ">{name}</h1>
+                        <div className=' flex justify-between items-center'>
+                            <h1 className="text-blue-900 text-xl title-font font-medium mb-1 ">{name}</h1>
+
+                        </div>
                         <h2 className="text-sm title-font text-gray-500 tracking-widest">Course Duration: {duration}</h2>
                         <div className="flex mb-4">
                             <span className="flex items-center">
@@ -33,15 +63,16 @@ const CourseDetail = () => {
                         <p className="leading-relaxed">{description}</p>
 
 
-                        <p className='mt-4'>
-                            <Link
-                                to={`/checkout/${id}`}
-                                className=" text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded w-full text-center ">Buy This Course</Link>
-                        </p>
+
 
                     </div>
                 </div>
             </div>
+            <p className='mb-4 text-center flex w-2/4 mx-auto'>
+                <Link
+                    to={`/checkout/${id}`}
+                    className=" text-white bg-blue-900 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded w-full text-center  ">Buy This Course</Link>
+            </p>
         </section>
     );
 };
